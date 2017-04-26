1a9c2959259b9d80806cf36b7473a50a0560a495
ginaienopoli <gienopoli@aol.com>
2017-04-26 15:11:53 -0400

    Add files via upload

--- Jquery-order/script.js
+++ Jquery-order/script.js
@@ -1,17 +1,3 @@
-.ui-controlgroup-vertical {
-      width: 150px;
-    }
-    .ui-controlgroup.ui-controlgroup-vertical > button.ui-button,
-    .ui-controlgroup.ui-controlgroup-vertical > .ui-controlgroup-label {
-      text-align: center;
-    }
-    #car-type-button {
-      width: 120px;
-    }
-    .ui-controlgroup-horizontal .ui-spinner-input {
-      width: 20px;
-    }
-  </style>
   <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
   <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
   <script>
