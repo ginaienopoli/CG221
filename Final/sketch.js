df51f94d998ca1abe5ff97809976331f1753bb28
ginaienopoli <gienopoli@aol.com>
2017-05-10 21:35:11 -0400

    Add files via upload

--- Final/sketch.js
+++ Final/sketch.js
@@ -1,59 +1,45 @@
 var king;
-var serv1 = "serv1.svg";
-var serv2 = "serv2.svg";
-var cheese = "cheese.svg";
-
+var kingopen;
+var serv1;
+var serv2;
+var cake;
+var cake1;
 
 function preload() {
   king = loadImage("king.svg");
+  kingopen = loadImage("kingopen.svg");
+  //animation must be in png format
+
+
+  //cake = loadAnimation("cake.png", "cake1.png");
+  //cake.playing = false;
+
+  //cake = loadImage("cake.svg");
+  //cake1 = loadImage("cake1.svg")
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight)
-  //king = loadImage("king.svg");
-  //king = new mousePressed();
-  // serv1 = new mousePressed();
-  // serv2 = new mousePressed();
-  // cheese = new mousePressed();
-  // y = height;
+
+  cake = createSprite(600, 600);
+  cake.addAnimation("shrink", "cake.png", "cake1.png");
+  cake.setCollider("circle", 0,0,64);
+  cake.onMouseOver = function() {
+  this.changeAnimation("shrink");
+  }
 
 }
 
 
 function draw() {
-  background(0);//yellow
-
+  background('#FFDC6B')//light yellow
+  //animation(cake, 500, 150);
+  image(king, mouseX-10, mouseY-10);
+  drawSprites();
 
   if (mouseIsPressed) {
-    image(king, mouseX, mouseY);
-  }
+    image(kingopen, mouseX-10, mouseY-10);
+    //cake.nextFrame();
 
-}
-
-// When the user clicks the mouse
-// function mousePressed() {
-//   // Check if mouse is inside the cheese
-//   var d = dist(mouseX, mouseY, 100, 100);
-//   if (d < 100) {
-//     // Pick new random color values
-//     serv1= false;
-//     serv2= false;
-//     cheese= y - 1;
-//
-//
-//     if (serv1= false){
-//       serv1= "serv2.svg"
-//     }
-//
-//     if (serv2= false){
-//       serv2= "serv1.svg"
-//     }
-//   }
-// }
-
-// When the user releases the mouse
-function mouseReleased() {
-  cheese= height +1;
-  serv1= true;
-  serv2= true;
+       }
 }
