e71181236e49bed77d20aff2c20376d32fd7259f
ginaienopoli <gienopoli@aol.com>
2017-05-11 21:09:36 -0400

    trying to fix cake mouse animation

--- Final/sketch.js
+++ Final/sketch.js
@@ -1,45 +1,39 @@
-var king;
-var kingopen;
-var serv1;
-var serv2;
-var cake;
-var cake1;
+var king, cake;
 
 function preload() {
-  king = loadImage("king.svg");
-  kingopen = loadImage("kingopen.svg");
+  // king = loadImage("king.svg");
+  // kingopen = loadImage("kingopen.svg");
   //animation must be in png format
-
-
-  //cake = loadAnimation("cake.png", "cake1.png");
-  //cake.playing = false;
-
-  //cake = loadImage("cake.svg");
-  //cake1 = loadImage("cake1.svg")
+  //
+  cake = loadAnimation("cake.png", "cake1.png");
+  cake.playing = false;
 }
 
 function setup() {
-  createCanvas(windowWidth, windowHeight)
+  createCanvas(windowWidth, windowHeight);
 
-  cake = createSprite(600, 600);
-  cake.addAnimation("shrink", "cake.png", "cake1.png");
-  cake.setCollider("circle", 0,0,64);
-  cake.onMouseOver = function() {
-  this.changeAnimation("shrink");
+  // cake = createSprite(600, 600);
+  // cake.addAnimation("shrink", "cake.png", "cake1.png");
+  // cake.setCollider("circle", 0,0,64);
+  // cake.onMouseOver = function() {
+  // this.changeAnimation("shrink");
   }
 
-}
 
 
 function draw() {
   background('#FFDC6B')//light yellow
-  //animation(cake, 500, 150);
-  image(king, mouseX-10, mouseY-10);
-  drawSprites();
+  animation(cake, 150, 150);
+  // image(king, mouseX-10, mouseY-10);
+  //drawSprites();
 
-  if (mouseIsPressed) {
-    image(kingopen, mouseX-10, mouseY-10);
-    //cake.nextFrame();
+  // if (mouseIsPressed) {
+  //   image(kingopen, mouseX-10, mouseY-10);
+  //   cake.nextFrame();
+  //
+  //      }
+}
 
-       }
+function mousePressed() {
+  cake.nextFrame();
 }
