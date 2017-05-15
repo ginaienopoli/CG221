d9419e4c0237cc326cb83ec2b8e478b67a2bad17
ginaienopoli <gienopoli@aol.com>
2017-05-15 02:06:50 -0400

    Update sketch.js

--- Final Test/sketch.js
+++ Final Test/sketch.js
@@ -1,49 +1,43 @@
 var king, cake, cake1;
 
-var mainArr = new Array;
-var imgArray = mainArr;
 
-mainArr[0] = new Image();
-mainArr[0].src = "cake.png";
-
-mainArr[1] = new Image();
-mainArr[1].src = "cake1.png";
-
-function newIndex(arrLength) {
-	var randIndex = Math.floor(arrLength * Math.random());
-	return randIndex;
-}
-
-var myIndex = newIndex(imgArray.length);
 
 function preload() {
-  king = loadImage("king.svg");
-  kingopen = loadImage("kingopen.svg");
-  //cake = loadImage("cake.png");
-  //cake1 = loadImage("cake1.png");
-  
+  king = loadImage("img/king.svg");
+  kingopen = loadImage("img/king1.svg");
+  cake = loadImage("img/cake.svg");
+  cake1 = loadImage("img/cake1.svg");
+  // cake[1] = loadImage("cake.png");
+  // cake[2] = loadImage("cake1.png");
+
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
- 
   }
-
-
 
 function draw() {
   background('#FFDC6B')//light yellow
-  image(cake, 500, 500, 200, 200);
-  image(king, mouseX-10, mouseY-10);
+  image(cake, 200, 200);
+  image(king, mouseX-50, mouseY-50);
 
 
   if (mouseIsPressed) {
-    var d = dist(mouseX, mouseY, 500, 500);
+
+    var d = dist(mouseX, mouseY, 200, 200);
   if (d < 100) {
-    myIndex = newIndex(imgArray.length);
+		if (this.src == 'cake') {
+        this.src = 'cake1';
+    } else if (this.src == 'cake1') {
+        this.src = 'cake';
+    } else {
+        alert('error');
+    	}
+		}
+		//for (var i=0; i<4; i
+			///cake (200,200 nums[i], nums[i]);
+		//cake = round(0, cake.length);
     //image(cake1, 500, 500, 200, 200);
-    image(kingopen, mouseX-10, mouseY-10);
-          }
-  return false;
-       }
+    image(kingopen, mouseX-50, mouseY-50);
+	}
 }
