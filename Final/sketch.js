a17f95628ca7632a209d469f9c0ad14a44b3851a
ginaienopoli <gienopoli@aol.com>
2017-05-15 21:57:32 -0400

    fixed everything done. ends to be centered though

--- Final/sketch.js
+++ Final/sketch.js
@@ -1,39 +1,64 @@
-var king, cake;
+var king, cake, cake1;
+var cakes = [];
+var drawcake;
+var cakeindex = 0;
+var drawking;
 
 function preload() {
-  // king = loadImage("king.svg");
-  // kingopen = loadImage("kingopen.svg");
-  //animation must be in png format
-  //
-  cake = loadAnimation("cake.png", "cake1.png");
-  cake.playing = false;
+  fontQuicksand = loadFont("Quicksand-Regular.otf");
+  king = loadImage("img/king.svg");
+  kingopen = loadImage("img/king1.svg");
+  cakes[0] = loadImage("img/cake.svg");
+  cakes[1] = loadImage("img/cake1.svg");
+  cakes[2] = loadImage("img/cake2.svg");
+  cakes[3] = loadImage("img/cake3.svg");
+  cakes[4] = loadImage("img/cake4.svg");
+  cakes[5] = loadImage("img/cake5.svg");
+  cakes[6] = loadImage("img/cake6.svg");
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-
-  // cake = createSprite(600, 600);
-  // cake.addAnimation("shrink", "cake.png", "cake1.png");
-  // cake.setCollider("circle", 0,0,64);
-  // cake.onMouseOver = function() {
-  // this.changeAnimation("shrink");
   }
-
-
 
 function draw() {
   background('#FFDC6B')//light yellow
-  animation(cake, 150, 150);
-  // image(king, mouseX-10, mouseY-10);
-  //drawSprites();
+  //text settings
+  textAlign(CENTER);
+  textSize(40);
+  textFont(fontQuicksand);
+  text("king duck is hungry", 570, 50);
+  textSize(32);
+  text("let him eat cake!", 580, 260);
+  fill('#2E3192');//blue
+  //cake settings
+  drawcake = cakes[cakeindex];
+  image(drawcake, 500, 50);
 
+  //king settings
+  //mouse is pressed goes by how long you hold down the click
   if (mouseIsPressed) {
-  //   image(kingopen, mouseX-10, mouseY-10);
-    cake.nextFrame();
-  //
-       }
-}
+     drawking = kingopen;
+     image(drawking, mouseX-50, mouseY-50);
+	 }
 
-// function mousePressed() {
-//   cake.nextFrame();
-// }
+   else{
+     drawking = king;
+     image(drawking, mouseX-50, mouseY-50);
+   }
+  }
+
+  //mouse clicked can only be a function and based off the # of clicks not how
+  //long the clicks are like mouse is pressed
+  function mouseClicked() {
+    //determines the space where the event will happen
+    var d = dist(mouseX, mouseY, 590, 100);
+    if (d < 100) {
+      //if the number for cakeindex is greater it will loop again
+      cakeindex++;
+        if (cakeindex > 6){
+          cakeindex = 0;
+        }
+      image(kingopen, mouseX-50, mouseY-50);
+      }
+    }
